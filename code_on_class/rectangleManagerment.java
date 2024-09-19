8b6091b5dab59a1039c02aebf38666ee160be1e6
tvhoang12 <HoangTV.B22AT129@stu.ptit.edu.vn>
2024-09-19 11:51:56 +0700

    new

---
+++ code_on_class/rectangleManagerment.java
@@ -0,0 +1,9 @@
+package code_on_class;
+
+public class rectangleManagerment {
+    public static void main(String[] args) {
+        rectangle hcn = new rectangle();
+        hcn.getInformation();
+        hcn.display();
+    }
+}
