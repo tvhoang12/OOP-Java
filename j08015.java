0ae35be92fb7d6ed44b40548d6850112e87a9263
tvhoang12 <HoangTV.B22AT129@stu.ptit.edu.vn>
2024-12-08 10:54:25 +0700

    new update 8/12

--- j08015.java
+++ j08015.java
@@ -1,34 +1,40 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
-public class j08015 {
+public class j08015 { 
+    
+
     public static void main(String [] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         while(t --> 0) {
             int n = sc.nextInt();
-            long k = sc.nextLong();
-            List<Long> a = new ArrayList<Long>();
-            for (int i = 0; i < n; i++) {
-                a.add(sc.nextLong());
-            }
-
-            Collections.sort(a);
-
-            int res = 0;
-
+            int k = sc.nextInt();
+            
+            int [] arr = new int[n];
+            HashMap<Integer, Integer> map = new HashMap<>();
             for(int i = 0; i < n; i++) {
-                if(Collections.binarySearch(a, k - a.get(i)) >= 0) {
-                    if(a.get(Collections.binarySearch(a, k - a.get(i))) <= a.get(i)) {
-                        res++;
-                    }
+                arr[i] = sc.nextInt();
+                if(map.get(arr[i]) == null) {
+                    map.put(arr[i], 1);
+                }
+                else map.put(arr[i], map.get(arr[i]) + 1);
+            }
+            int count = 0;
+            for(int i = 0; i < n; i++) {
+                if(map.get(k - arr[i]) != null) {
+                    count += map.get(k - arr[i]);
+                }
+                if(k - arr[i] == arr[i]) {
+                    count -= 1;
                 }
             }
-            System.out.println(res);
+
+            System.out.println(count / 2);
+            
         }
-        sc.close();
-    }
+        
+    }   
     
 }
